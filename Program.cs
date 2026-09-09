03dd1b6e1b1d5e8d4b99045530a7f193bdeb253d
VonRiddarn <vonriddarn@gmail.com>
2026-09-09 19:04:28 +0200

    chore: file formatting

--- Program.cs
+++ Program.cs
@@ -5,21 +5,19 @@ namespace ConsoleAtHome;
 
 class Program
 {
-	static void Main(string[] args)
+	static void Main()
 	{
 		Console.WriteLine("Hello, World!");
 
 		int age = Cah.ParseLine<int>("Enter age: ");
-
 		Console.WriteLine($"You are {age} years old.");
 
 		bool likesDogs = Cah.ParseYesNo("Do you like dogs (y / n): ");
-
 		Console.WriteLine(likesDogs ? "Nice, me too!!" : "That's a shame, they're cute!");
 
 		string[] animals = ["dogs", "cats", "birds", "mice", "sheep"];
-		int index = Cah.ParseCustom("What's your favorite animal: ", animals);
 
+		int index = Cah.ParseCustom("What's your favorite animal: ", animals);
 		Console.WriteLine($"Cool! I like {animals[index]} too!");
 	}
 }
