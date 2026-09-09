3675bbce1c030c6d9c0f403bdab7ac76d4570f7d
VonRiddarn <vonriddarn@gmail.com>
2026-09-09 19:35:37 +0200

    refactor: move input related methods into nested static class for scope control

--- Program.cs
+++ Program.cs
@@ -9,21 +9,21 @@ class Program
 	{
 		Console.WriteLine("Hello, World!");
 
-		int age = Cah.ParseLine<int>("Enter age: ");
+		int age = Cah.Input.ParseLine<int>("Enter age: ");
 		Console.WriteLine($"You are {age} years old.");
 
-		bool likesDogs = Cah.ParseYesNo("Do you like dogs (y / n): ");
+		bool likesDogs = Cah.Input.ParseYesNo("Do you like dogs (y / n): ");
 		Console.WriteLine(likesDogs ? "Nice, me too!!" : "That's a shame, they're cute!");
 
 		string[] animals = ["dogs", "cats", "birds", "mice", "sheep"];
 
-		int index = Cah.ParseCustom("What's your favorite animal: ", animals);
+		int index = Cah.Input.ParseCustom("What's your favorite animal: ", animals);
 		Console.WriteLine($"Cool! I like {animals[index]} too!");
 
 		for (int i = 0; i < animals.Length; i++)
 			Console.WriteLine($"{i + 1}) {animals[i]}");
 
-		string animal = Cah.SelectFromIndex<string>("Choose: ", animals, indexCorrection: -1);
+		string animal = Cah.Input.SelectFromIndex<string>("Choose: ", animals, indexCorrection: -1);
 		Console.WriteLine($"Choice = {animal}.");
 	}
 }
